6abbee9df5898cf9369a45ca270bef9bd9948d61
Glitch (tin-cooperative-explanation) <none>
2021-06-17 12:40:32 +0000

    👭🐯 Updated with Glitch

---
+++ config.js
@@ -0,0 +1,36 @@
+const config = {
+    "ownerID": "573548185428164630", //kendi IDınızı yazınız
+    "admins": ["573548185428164630"],
+    "support": ["573548185428164630"],
+    "token": "NzU0MDQxNDUwNTU0OTgyNTUw.X1u90w.lANUOzmIPn67OHktT118UbdAqrY", //botunuzun tokenini yazınız
+    "dashboard" : {
+      "oauthSecret": "XYntZXWxwFK_DU4gTgHU5yw6rqLdxZq_", //botunuzun secretini yazınız
+      "callbackURL": `https://paint-marked-attempt.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "sessionSecret": "super-secret-session-thing", //kalsın
+      "domain": "https://paint-marked-attempt.glitch.me/", //site URLnizi yazınız!
+          "port": process.env.PORT
+    }
+  };
+  
+  module.exports = config;
+
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+// PROJEYİ GİZLİYE ALMAYI UNUTMA
+
+//GİZLYİE ALMASANIZ SUNUCUNUZ PATLIYABİLİR
